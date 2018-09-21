6c695331d2b82b66e312a32118c6c3825e3778c2
Saptarshi Chatterjee <diesel@falcon.local>
2018-09-21 00:05:08 -0500

    login clean up

--- client/src/containers/Login/Login.js
+++ client/src/containers/Login/Login.js
@@ -33,61 +33,19 @@ class Login extends Component {
     super(props);
     this.state = {
       valid: 0,
-      register_name: '',
-      register_email: '',
-      register_pwd: '',
       login_email: '',
       login_pwd: '',
-      showRegister: true,
-      reg_show_password: false,
-      login_show_password: false,
     };
   }
 
   componentDidMount() {
     const { match: { params: { islogin } } } = this.props;
-    if (islogin === 'login') {
-      this.setState({ showRegister: false });
-    }
-  }
-
-  handleRegister = (event) => {
-    event.preventDefault();
-    
-    const { register_name, register_email, register_pwd } = this.state;
-
-    if (register_name.length < 3 || register_name.indexOf('asds') !== -1) {
-      this.setState({ valid: 1});
-      return;
-    }
-    if (!register_email.length || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(register_email)) {
-      this.setState({ valid: 2});
-      return;    
-    }
-    if (register_pwd.length < 5) {
-      this.setState({ valid: 3});
-      return;
-    }
-    
-    this.setState({ valid: -1});
-    this.props.register({ email:register_email, password:register_pwd, name:register_name, accounttype: 1 });
   }
 
   handleChangeInfo = name => event  => {
     this.setState({ [name]: event.target.value, valid: 0 });
   }
 
-  handleRegisterSuccess = () => {
-    this.props.history.push('/dashboard');
-  }
-
-  handleClickShowPassword = () => {
-    this.setState(state => ({ showPassword: !state.showPassword }));
-  };
-
-  handleMouseDownPassword = event => {
-    event.preventDefault();
-  };
 
   handleSignin = (event) => {
     event.preventDefault();
@@ -107,7 +65,6 @@ class Login extends Component {
       validationStyle = 'success';
     else
       validationStyle = 'validationTrue';
-     
     return (
       <div className='loginPage'>
         {
@@ -139,17 +96,6 @@ class Login extends Component {
                     type={this.state.showPassword ? 'text' : 'password'}
                     value={this.state.login_pwd}
                     onChange={this.handleChangeInfo('login_pwd')}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="Toggle password visibility"
-                          onClick={this.handleClickShowPassword}
-                          onMouseDown={this.handleMouseDownPassword}
-                        >
-                          {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
                   />
                 </FormControl>
 
@@ -161,14 +107,6 @@ class Login extends Component {
                 >
                   Login
                 </Button>
-
-                <div className='toggleText'>
-                  Don't have an account?
-                  <Link to="/">
-                    <span>&nbsp;Register</span>
-                  </Link>
-                </div>
-
                 {
                   <div className={validationStyle}>
                     <i className="fa fa-exclamation-triangle valid__icon"></i>
@@ -189,12 +127,10 @@ class Login extends Component {
         {user &&
         <div>
           <p>You are currently logged in as {user.name}.</p>
-          <h1> Account Details </h1>
           <div className="card">
-            <img className="card-img-top" src="https://iffhs.de/wp-content/uploads/2017/12/lionel-messi.jpg" alt="ProfilePic" style={{ width: 150, height: 150 }}/>
             <div className="card-body">
               <h5 className="card-title">{user.name}</h5>
-              <p className="card-text"> Your account type is <strong>{user.account_type === 1 ? ' Employer' : 'Test Taker'}</strong> . Thank you for registering with us</p>
+              <p className="card-text"> Your account type is <strong>{user.account_type === 1 ? ' Admin ' : 'Quiz Taker'}</strong> . Thank you for registering with us</p>
               <a href="/" onClick={doLogout} className="btn btn-primary" >Log out</a>
             </div>
           </div>
@@ -206,7 +142,6 @@ class Login extends Component {
 }
 
 Login.propTypes = {
-  register: PropTypes.func,
   doLogout: PropTypes.func,
   doSignin: PropTypes.func,
   status: PropTypes.object,
